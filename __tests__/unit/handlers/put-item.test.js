fba37c447e45c4b775424546aeaa3810b9378359
Guillaume JUST <just.guillaume@hotmail.fr>
2020-04-05 15:41:04 +0200

    Create Stop all virus project

--- __tests__/unit/handlers/put-item.test.js
+++ __tests__/unit/handlers/put-item.test.js
@@ -6,40 +6,96 @@ const lambda = require('../../../src/handlers/put-item.js');
 
 // This includes all tests for putItemHandler
 describe('Test putItemHandler', () => {
-    let putSpy;
+    let batchWriteSpy;
 
     // One-time setup and teardown, see more in https://jestjs.io/docs/en/setup-teardown
     beforeAll(() => {
-        // Mock DynamoDB put method
+        // Mock DynamoDB batchWrite method
         // https://jestjs.io/docs/en/jest-object.html#jestspyonobject-methodname
-        putSpy = jest.spyOn(dynamodb.DocumentClient.prototype, 'put');
+        batchWriteSpy = jest.spyOn(dynamodb.DocumentClient.prototype, 'batchWrite');
     });
 
     // Clean up mocks
     afterAll(() => {
-        putSpy.mockRestore();
+        batchWriteSpy.mockRestore();
     });
 
     // This test invokes putItemHandler and compares the result
     it('should add id to the table', async () => {
-        // Return the specified value whenever the spied put function is called
-        putSpy.mockReturnValue({
+        // Return the specified value whenever the spied batchWrite function is called
+        batchWriteSpy.mockReturnValue({
             promise: () => Promise.resolve('data'),
         });
 
         const event = {
             httpMethod: 'POST',
-            body: '{"id":"id1","name":"name1"}',
+            body: '{"ids":["test", "test1", "test2"]}',
         };
 
         // Invoke putItemHandler()
         const result = await lambda.putItemHandler(event);
         const expectedResult = {
             statusCode: 200,
-            body: event.body,
+            body: JSON.stringify({
+            	added: 3,
+				success: true
+			}),
         };
 
         // Compare the result with the expected result
         expect(result).toEqual(expectedResult);
     });
+
+	// This test invokes putItemHandler and compares the result
+	it('should nothing if body is missing', async () => {
+		// Return the specified value whenever the spied batchWrite function is called
+		batchWriteSpy.mockReturnValue({
+			promise: () => Promise.resolve('data'),
+		});
+
+		const event = {
+			httpMethod: 'POST',
+		};
+
+		// Invoke putItemHandler()
+		const result = await lambda.putItemHandler(event);
+		const expectedResult = {
+			statusCode: 200,
+			body: JSON.stringify({
+				"added": 0,
+				"message": "Nothing to add",
+				"success": true,
+			}),
+		};
+
+		// Compare the result with the expected result
+		expect(result).toEqual(expectedResult);
+	});
+
+	// This test invokes putItemHandler and compares the result
+	it('should nothing if body is not an array', async () => {
+		// Return the specified value whenever the spied batchWrite function is called
+		batchWriteSpy.mockReturnValue({
+			promise: () => Promise.resolve('data'),
+		});
+
+		const event = {
+			httpMethod: 'POST',
+			body: '{"ids":"sdfsd"}',
+		};
+
+		// Invoke putItemHandler()
+		const result = await lambda.putItemHandler(event);
+		const expectedResult = {
+			statusCode: 200,
+			body: JSON.stringify({
+				"added": 0,
+				"message": "Nothing to add",
+				"success": true,
+			}),
+		};
+
+		// Compare the result with the expected result
+		expect(result).toEqual(expectedResult);
+	});
 });
--- src/handlers/compare-infected-items.js
+++ src/handlers/compare-infected-items.js
@@ -1,6 +1,6 @@
 // Create clients and set shared const values outside of the handler
 
-// Create a DocumentClient that represents the query to add an item
+// Create a DocumentClient that represents the query to get all items
 const dynamodb = require('aws-sdk/clients/dynamodb');
 
 const docClient = new dynamodb.DocumentClient();
@@ -9,48 +9,41 @@ const docClient = new dynamodb.DocumentClient();
 const tableName = process.env.SAMPLE_TABLE;
 
 /**
- * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
-exports.putItemHandler = async (event) => {
-	//stopallvirus/api-backend
-    const { body, httpMethod } = event;
+exports.compareInfectedItemsHandler = async (event) => {
+    const { httpMethod, body } = event;
     if (httpMethod !== 'POST') {
-        throw new Error(`postMethod only accepts POST method, you tried: ${httpMethod} method.`);
+        throw new Error(`compareInfectedItems only accept POST method, you tried: ${httpMethod}`);
     }
 	// All log statements are written to CloudWatch by default. For more information, see
 	// https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
 	console.log('received:', JSON.stringify(event));
 
-    // Get id and name from the body of the request
-	const ids = getIdsFromBody(body);
+	const ids = getIdsFromParameters(body);
 
 	if (!ids) {
-		return formatResponse({added: 0, message: "Nothing to add"});
+		return formatResponse({message: "Nothing to compare", item: []});
 	}
 
-	let items = ids.map(id => {
-		return {
-			PutRequest: {
-				Item: {
-					id: id
-				}
-			}
-		};
-	});
-
 	let RequestItems = {};
-	RequestItems[tableName] = items;
+
+	RequestItems[tableName] = {Keys: ids.map(id => {
+			return {id}
+		})
+	};
 
 	let params = {
 		RequestItems
 	};
 
-    await docClient.batchWrite(params).promise();
+	const { Responses } = await docClient.batchGet(params).promise();
+	const Items = Responses[tableName];
 
-	return formatResponse({added: ids.length});
+    return formatResponse({items: Items});
 };
 
-function getIdsFromBody(data) {
+function getIdsFromParameters(data) {
 	let ids;
 	if (data) {
 		let dataJson = data;
